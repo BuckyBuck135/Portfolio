4cb7daeb3077c6a2701aa683d2f1b94ce9deabda
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-11 18:39:34 +0200

    data

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -18,12 +18,10 @@ export const data = [
             "/images/kasa/TABLET-kasa-location.netlify.app_a-propos.png",
             "/images/kasa/kasa-location.netlify.app_a-propos.png",
             "/images/kasa/localhost_3000_fiche-logement_b9123946(iPhone%2012%20Pro).png",
-
-            
         ],
         heading: "Kasa",
-        subheading: "location d’appartements entre particuliers",
-        description: "lorem ipsum blah blah",
+        subheading: "Location d’appartements entre particuliers",
+        description: "Ce projet OpenClassrooms utilise React Router pour la navigation entre les 4 pages et met en exergue la bonne pratique de composants réutilisables. J'y ai également inclus une couche services pour la future intégration d'une API.",
         stack: [
             "fa-brands fa-react",
             "fa-brands fa-css3-alt"
@@ -37,7 +35,32 @@ export const data = [
     }, 
     {
         id: nanoid(),
-        cover: "/images/sophie-bluel/cover.jpg",
+        cover: "/images/grimoire/mockup.jpg",
+        pictures: [
+            "/images/grimoire/cover.jpg",
+            "/images/grimoire/book-detail.jpg",
+            "/images/grimoire/laptop2.jpg",
+            "/images/grimoire/tablet.jpg",
+        ],
+        heading: "Mon Vieux Grimoire",
+        subheading: "Site communautaire de notations de livres",
+        description: "Pour ce projet OpenClassrooms, j'ai développé le back-end de ce site avec le stack MERN. Cette API REST permet la réalisation d'opérations CRUD telles que l'ajout de livres et la notation. La sécurisation des routes protégées est effectuée avec JWT",
+        stack: [
+            "fa-brands fa-react",
+            "fa-brands fa-css3-alt",
+            "fa-brands fa-node-js"
+        ],
+        liveUrl: "https://kasa-location.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/projet-6---kasa",
+        tags: [
+            "NODE",
+            "EXPRESS",
+            "MONGODB"
+        ]
+    }, 
+    {
+        id: nanoid(),
+        cover: "/images/sophie-bluel/mockup.jpg",
         pictures: [
             "/images/sophie-bluel/cover.jpg",
             "/images/sophie-bluel/login.jpg",
@@ -46,12 +69,13 @@ export const data = [
             "/images/sophie-bluel/modal1.jpg",
             "/images/sophie-bluel/modal2.jpg",
         ],
-        heading: "Sophie Bluel - Architecte",
-        subheading: "Projet OpenClassrooms",
-        description: "lorem ipsum blah blah",
+        heading: "Sophie Bluel",
+        subheading: "Portfolio pour architecte d'intérieur",
+        description: "Pour ce projet OpenClassrooms, j'ai développé le front-end et intégré les requêtes API pour communiquer avec le back-end existant. Une page login permet à l'administrateur de se connecter puis d'utiliser des opérations CRUD pour la gestion de ses travaux.",
         stack: [
             "fa-brands fa-html5",
-            "fa-brands fa-css3-alt"
+            "fa-brands fa-css3-alt",
+            "fa-brands fa-js"
         ],
         liveUrl: "https://kasa-location.netlify.app/",
         repoUrl: "https://github.com/BuckyBuck135/Projet-3---Portfolio-architecte-sophie-bluel",
@@ -63,7 +87,7 @@ export const data = [
     },  
     {
         id: nanoid(),
-        cover: "/images/sliced/cover.jpg",
+        cover: "/images/sliced/mockup.jpg",
         pictures: [
             "/images/sliced/cover.jpg",
             "/images/sliced/order.jpg",
@@ -72,13 +96,38 @@ export const data = [
         ],
         heading: "Sliced",
         subheading: "Commande de pizzas en ligne",
-        description: "Sliced, the home of the true pizza slice, est basé sur un projet solo Scrimba frontend career path pour le module 5 Essential JS. J'y ai ajouté mon propre style, avec un logo Illustrator fait sur mesure, et mis en application des concepts tels que l'utilisation de data attributes, .map() et validation de formulaire.",
+        description: "Sliced, the home of the true pizza slice, est une adaptation personalisée d'un projet solo Scrimba frontend career path pour le module 5 Essential JS. J'y ai ajouté mon propre style, avec un logo Illustrator fait sur mesure, et mis en application des concepts tels que l'utilisation de data attributes, .map() et validation de formulaire.",
+        stack: [
+            "fa-brands fa-html5",
+            "fa-brands fa-css3-alt",
+            "fa-brands fa-js"
+        ],
+        liveUrl: "https://sliced-restaurant.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/Food-ordering-app",
+        tags: [
+            "HTML",
+            "CSS",
+            "JS"
+        ]
+    },  
+    {
+        id: nanoid(),
+        cover: "/images/booki/mockup.jpg",
+        pictures: [
+            "/images/booki/desktop.jpg",         
+            "/images/booki/mobile.jpg",         
+            "/images/booki/tablet.jpg",         
+            "/images/booki/activites.jpg",         
+        ],
+        heading: "Booki",
+        subheading: "Trouvez votre hébergement pour des vacances de rêve",
+        description: "Booki fut mon premier projet OpenClassrooms. En partantd'un design Figma, jai intégré cette interface totalement responsive (desktop, tablette, mobile) en HTML et CSS. Cette version ne prévoyant pas de JS, les filtres ne sont donc pas fonctionnels.",
         stack: [
             "fa-brands fa-html5",
             "fa-brands fa-css3-alt"
         ],
-        liveUrl: "https://github.com/BuckyBuck135/Food-ordering-app",
-        repoUrl: "https://github.com/BuckyBuck135/Food-ordering-app",
+        liveUrl: "https://booki-vacances.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/Projet-2---Integration-site-agence-de-voyage",
         tags: [
             "HTML",
             "CSS",
@@ -86,7 +135,7 @@ export const data = [
     },   
     {
         id: nanoid(),
-        cover: "/images/password-generator/cover.jpg",
+        cover: "/images/password-generator/mockup.jpg",
         pictures: [
             "/images/password-generator/cover.jpg",
             "/images/password-generator/mobile.jpg",
@@ -103,6 +152,31 @@ export const data = [
         ],
         liveUrl: "https://generate-unique-passwords.netlify.app/",
         repoUrl: "https://github.com/BuckyBuck135/Password-Generator",
+        tags: [
+            "HTML",
+            "CSS",
+            "JS"
+        ]
+    },
+    {
+        id: nanoid(),
+        cover: "/images/currency-converter/mockup.jpg",
+        pictures: [
+            "/images/currency-converter/laptop.jpg",
+            "/images/currency-converter/mobile.jpg",
+            "/images/currency-converter/tablet.jpg"
+            
+        ],
+        heading: "₡urrency ₡onverter",
+        subheading: "Convertisseur EUR / HKD / AUD / MYR",
+        description: "Basé sur un solo projet de Scrimba, cette web app m'a initié aux principes Don't Repeat Yourself grâce à l'utilisation de fonctions utilisants des paramètres. J'ai également appliqué l'adage 'never trust the user' et ai appliqué des contrôles de l'input utilisateur.",
+        stack: [
+            "fa-brands fa-html5",
+            "fa-brands fa-css3-alt",
+            "fa-brands fa-js"
+        ],
+        liveUrl: "https://currency-converter-eur.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/currency-converter",
         tags: [
             "HTML",
             "CSS",
