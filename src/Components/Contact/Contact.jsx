dde31d10125d16f2e5953d8b573029d119078e76
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-12 16:54:54 +0200

    contact form

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { useForm } from 'react-hook-form';
+
+
+import "./Contact.css"
+import ProjectCard from "../Card/ProjectCard";
+
+
+export default function Contact() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <section className="section--contact">
+        <svg preserveAspectRatio="none" viewBox="0 0 100 102" height="75" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0 L50 100 L100 0 Z" fill="#242424" stroke="#242424"></path>
+        </svg>
+        <div className="container"> 
+            <h2 className="section--heading"><span className="section--number">03.</span> Contact</h2> 
+            <h3 className="contact--subheading">Une question ? Un projet à réaliser ensemble ?</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Nom :" {...register("Name", {required: true, maxLength: 100})} />
+                <input type="text" placeholder="Email :" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+                <textarea placeholder="Votre message : " {...register("Message", {required: true})} />
+                <input type="submit" />
+            </form>
+        </div>
+    </section>
+  );
+}
