7da50efe7b1fee8164db89e6190eae8f093f57ed
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-16 17:19:18 +0200

    updated links

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -17,23 +17,23 @@ export default function Header() {
     })
 
     return (
-        <header>
+        <header id="header">
             <nav>
                 <div className="nav--logo">
                     {isDesktop ? (<img className="logo" src="logo-accent.svg" />) : (<img className="logo" src="logo.svg" />)}
                 </div>
                 <div className="nav--buttons">
                     <ul>
-                        <li><a href="#a-propos" className="nav--button"><FontAwesomeIcon icon={faUserGraduate} /></a></li>
-                        <li><a href="#projets"className="nav--button"><FontAwesomeIcon icon={faLaptopFile} /></a></li>
-                        <li><a href="#contact" className="nav--button"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+                        <li><a href="#section--about" className="nav--button"><FontAwesomeIcon icon={faUserGraduate} /></a></li>
+                        <li><a href="#section--projects" className="nav--button"><FontAwesomeIcon icon={faLaptopFile} /></a></li>
+                        <li><a href="#section--contact" className="nav--button"><FontAwesomeIcon icon={faEnvelope} /></a></li>
                     </ul>
                 </div>
                 <div className="nav--links">
                     <ul>
-                        <li><a href="#a-propos" className="nav--text"><span className="nav--span">01.</span>A propos</a></li>
-                        <li><a href="#projets" className="nav--text"><span className="nav--span">02.</span>Projets</a></li>
-                        <li><a href="#contact" className="nav--text"><span className="nav--span">03.</span>Contact</a></li>
+                        <li><a href="#section--about" className="nav--text"><span className="nav--span">01.</span>A propos</a></li>
+                        <li><a href="#section--projects" className="nav--text"><span className="nav--span">02.</span>Projets</a></li>
+                        <li><a href="#section--contact" className="nav--text"><span className="nav--span">03.</span>Contact</a></li>
                     </ul>
                 </div>
             </nav>
