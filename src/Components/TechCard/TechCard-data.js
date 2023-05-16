b3fc5a59c6be5bf837682a510d5d1d5e5db72a17
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-16 17:19:50 +0200

    data

--- src/Components/TechCard/TechCard-data.js
+++ src/Components/TechCard/TechCard-data.js
@@ -1,24 +1,24 @@
-import { faTruckFast, faLightbulb, faMobileScreenButton, faWandMagicSparkles} from '@fortawesome/free-solid-svg-icons'
+import { faTruckFast, faLightbulb, faMobileScreenButton, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 
 export default [
     {
         icon: faTruckFast,
         heading: "Rapides",
-        text: "Des sites performants, des temps de chargement éclairs."
+        text: "Des sites fonctionnellement performants, des temps de chargement éclairs."
     },
     {
         icon: faLightbulb,
         heading: "Intuitifs",
-        text: "Construits pour une expérience utilisateur optimale."
+        text: "Construits pour une expérience utilisateur optimale et une navigation intuitive."
     },
     {
         icon: faMobileScreenButton,
         heading: "Responsive",
-        text: "Mes sites s'adaptent à toutes tailles d'écran."
+        text: "Mes sites s'adaptent à toutes tailles d'écran et pensés mobile-first."
     },
     {
-        icon: faWandMagicSparkles,
-        heading: "Dynamiques",
-        text: "Quelques tours de magie pour des sites captivants !"
+        icon: faMagnifyingGlass,
+        heading: "SEO",
+        text: "Optimisés pour les moteurs de recherche pour encore plus de clients."
     },
 ]
