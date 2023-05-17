edefc48d8bd29b10befcf122408c2d5bc4bb666c
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-17 16:46:39 +0200

    data

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -6,12 +6,12 @@ import { nanoid } from 'nanoid'
 // JS "fa-brands fa-js"
 // NODE "fa-brands fa-node-js"
 // REACT "fa-brands fa-react"
+// SEO "fa-solid fa-magnifying-glass"
 
 export const data = [
     
     {
         id: nanoid(),
-        cover: "/images/kasa/mockup.jpg",
         pictures: [
             "/images/kasa/DEKSTOP-localhost_3000_fiche-logement_c67ab8a7.png",
             "/images/kasa/TABLET-kasa-location.netlify.app_a-propos.png",
@@ -34,7 +34,6 @@ export const data = [
     }, 
     {
         id: nanoid(),
-        cover: "/images/grimoire/mockup.jpg",
         pictures: [
             "/images/grimoire/cover.jpg",
             "/images/grimoire/book-detail.jpg",
@@ -49,8 +48,8 @@ export const data = [
             "fa-brands fa-css3-alt",
             "fa-brands fa-node-js"
         ],
-        liveUrl: "https://kasa-location.netlify.app/",
-        repoUrl: "https://github.com/BuckyBuck135/projet-6---kasa",
+        liveUrl: "https://monvieuxgrimoire.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/Mon-Vieux-Grimoire--backend",
         tags: [
             "NODE",
             "EXPRESS",
@@ -59,7 +58,6 @@ export const data = [
     }, 
     {
         id: nanoid(),
-        cover: "/images/sophie-bluel/mockup.jpg",
         pictures: [
             "/images/sophie-bluel/cover.jpg",
             "/images/sophie-bluel/login.jpg",
@@ -76,7 +74,7 @@ export const data = [
             "fa-brands fa-css3-alt",
             "fa-brands fa-js"
         ],
-        liveUrl: "https://kasa-location.netlify.app/",
+        liveUrl: "https://sophiebluelarchitecte.netlify.app/",
         repoUrl: "https://github.com/BuckyBuck135/Projet-3---Portfolio-architecte-sophie-bluel",
         tags: [
             "HTML",
@@ -86,27 +84,29 @@ export const data = [
     },  
     {
         id: nanoid(),
-        cover: "/images/sliced/mockup.jpg",
         pictures: [
-            "/images/sliced/cover.jpg",
-            "/images/sliced/order.jpg",
-            "/images/sliced/form.jpg",
-            "/images/sliced/pay.jpg",          
+            "/images/nina-carducci/cover.jpg",
+            "/images/nina-carducci/lighthouse.jpg",
+            "/images/nina-carducci/gallery.jpg",
+            "/images/nina-carducci/services.jpg",
+            "/images/nina-carducci/contact.jpg",          
         ],
-        heading: "Sliced",
-        subheading: "Commande de pizzas en ligne",
-        description: "Sliced, the home of the true pizza slice, est une adaptation personalisée d'un projet solo Scrimba frontend career path pour le module 5 Essential JS. J'y ai ajouté mon propre style, avec un logo Illustrator fait sur mesure, et mis en application des concepts tels que l'utilisation de data attributes, .map() et validation de formulaire.",
+        heading: "Nina Carducci",
+        subheading: "Débogage, optimisation et SEO",
+        description: "Dans le cadre de ce projet OpenClassrooms, j'ai effectué un travail à plusieurs niveaux sur le site de cette photographe. Tout d'abord, l'optimisation des images du site : des images trop lourdes rendaient la page très longue à charger. Ensuite, le débogage du code et l'optimisation de sa structure afin d’améliorer le chargement et l’accessibilité du site. Finalement, un travail de SEO pour améliorer le positionnement du site dans les résultats de moteurs de recherche.",
         stack: [
             "fa-brands fa-html5",
             "fa-brands fa-css3-alt",
-            "fa-brands fa-js"
+            "fa-brands fa-js",
+            "fa-solid fa-magnifying-glass"
         ],
-        liveUrl: "https://sliced-restaurant.netlify.app/",
-        repoUrl: "https://github.com/BuckyBuck135/Food-ordering-app",
+        liveUrl: "https://nina-carducci-photographe.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/Projet-5---nina-carducci.github.io",
         tags: [
             "HTML",
             "CSS",
-            "JS"
+            "JS",
+            "SEO"
         ]
     },  
     {
@@ -131,10 +131,33 @@ export const data = [
             "HTML",
             "CSS",
         ]
-    },   
+    }, 
     {
         id: nanoid(),
-        cover: "/images/password-generator/mockup.jpg",
+        pictures: [
+            "/images/sliced/cover.jpg",
+            "/images/sliced/order.jpg",
+            "/images/sliced/form.jpg",
+            "/images/sliced/pay.jpg",          
+        ],
+        heading: "Sliced",
+        subheading: "Commande de pizzas en ligne",
+        description: "Sliced, the home of the true pizza slice, est une adaptation personalisée d'un projet solo Scrimba frontend career path pour le module 5 Essential JS. J'y ai ajouté mon propre style, avec un logo Illustrator fait sur mesure, et mis en application des concepts tels que l'utilisation de data attributes, .map() et validation de formulaire.",
+        stack: [
+            "fa-brands fa-html5",
+            "fa-brands fa-css3-alt",
+            "fa-brands fa-js"
+        ],
+        liveUrl: "https://sliced-restaurant.netlify.app/",
+        repoUrl: "https://github.com/BuckyBuck135/Food-ordering-app",
+        tags: [
+            "HTML",
+            "CSS",
+            "JS"
+        ]
+    },  
+    {
+        id: nanoid(),
         pictures: [
             "/images/password-generator/cover.jpg",
             "/images/password-generator/mobile.jpg",
@@ -143,7 +166,7 @@ export const data = [
         ],
         heading: "Générateur de mots de passe",
         subheading: "Never use an insecure password again",
-        description: "Ce projet Scrimba couvre les bases de JS telles que la manipulation du DOM et les boucles for. J'y ai ajouté un color picker en CSS pur et un toast pour la copie des mots de passe dans le presse-papiers.",
+        description: "Ce projet Scrimba couvre les bases de JS telles que la manipulation du DOM et les boucles for. J'y ai ajouté un color picker codé en CSS pur ainsi qu'une fonction de copie du mot de passe dans le presse-papiers. Un pop-up indique la copie réussie pour une meilleure expérience utilisateur.",
         stack: [
             "fa-brands fa-html5",
             "fa-brands fa-css3-alt",
@@ -156,30 +179,30 @@ export const data = [
             "CSS",
             "JS"
         ]
-    },
+    }
+    ,
     {
         id: nanoid(),
-        cover: "/images/currency-converter/mockup.jpg",
         pictures: [
-            "/images/currency-converter/laptop.jpg",
-            "/images/currency-converter/mobile.jpg",
-            "/images/currency-converter/tablet.jpg"
+            "/images/menumaker/cover.jpg",
+            "/images/menumaker/veille.jpg",
+            "/images/menumaker/flowchart.jpg",
+            "/images/menumaker/kanban.jpg",
+            "/images/menumaker/table.jpg",
             
         ],
-        heading: "₡urrency ₡onverter",
-        subheading: "Convertisseur EUR / HKD / AUD / MYR",
-        description: "Basé sur un solo projet de Scrimba, cette web app m'a initié aux principes Don't Repeat Yourself grâce à l'utilisation de fonctions utilisants des paramètres. J'ai également appliqué l'adage 'never trust the user' et ai appliqué des contrôles de l'input utilisateur.",
+        heading: "Menu Maker by Qwenta",
+        subheading: "Planification et gestion de projet",
+        description: "Ce projet OpenClassrooms est particulier puisqu'il ne concerne pas la programmation, mais la planification et la gestion de projet en trois axes : développer un système de veille technologique afin de trouver les solutions techniques aux besoins fonctionnels; en parallèle, déterminer les spécifications techniques du site Menu Maker by Qwenta; finalement, organiser la gestion de projet du développement du site.",
         stack: [
-            "fa-brands fa-html5",
-            "fa-brands fa-css3-alt",
-            "fa-brands fa-js"
+            ""
         ],
-        liveUrl: "https://currency-converter-eur.netlify.app/",
-        repoUrl: "https://github.com/BuckyBuck135/currency-converter",
+        liveUrl: "",
+        repoUrl: "",
         tags: [
-            "HTML",
-            "CSS",
-            "JS"
+            "Figma",
+            "Notion",
+            "Miro"
         ]
     },
 ]
