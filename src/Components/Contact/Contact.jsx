cb4ef5fcc056a22ea59d4f714c1016692742050c
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-22 14:10:21 +0200

    animating contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,15 +1,29 @@
 import React from "react"
-import { useForm } from 'react-hook-form';
-
-
 import "./Contact.css"
-import ProjectCard from "../ProjectCard/ProjectCard";
-
+import { useLayoutEffect } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Contact() {
-
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  useLayoutEffect(() => {
+    // create our context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
+    let ctx = gsap.context(() => {
+      const inputs = gsap.utils.toArray('.staggered');
+      inputs.forEach(input => {
+                gsap.to(input, { 
+                x: 0,
+                opacity: 1,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: ".section--contact .container",
+                    toggleActions: "play none play reset"
+                }
+                })
+            });
+        
+    });  
+    return () => ctx.revert(); // cleanup
+    
+    }, []); // <- empty dependency Array so it doesn't re-run on every render
   
   return (
     <section id="section--contact" className="section--contact">
@@ -23,12 +37,12 @@ export default function Contact() {
             <form name="contact" method="POST" data-netlify="true">
                 <input aria-label="contact" type="hidden" name="form-name" value="contact" />
                 <label className="visually-hidden"></label>
-                <input aria-label="nom" className="form--input" type="text" name="name" placeholder="Nom" required />
+                <input aria-label="nom" className="form--input staggered X-3" type="text" name="name" placeholder="Nom" required />
                 <label className="visually-hidden"></label>
-                <input aria-label="email" className="form--input" type="text" name="email" placeholder="Email" required />
+                <input aria-label="email" className="form--input staggered X3" type="text" name="email" placeholder="Email" required />
                 <label className="visually-hidden"></label>
-                <textarea aria-label="envoyer" className="form--input" name="message" placeholder="Votre message" required />
-                <button type="submit" className="timeBombButton">
+                <textarea aria-label="envoyer" className="form--input staggered X-3" name="message" placeholder="Votre message" required />
+                <button type="submit" className="timeBombButton staggered X3">
                   <span></span><span></span><span></span><span></span>
                   Envoyer</button>
             </form>
