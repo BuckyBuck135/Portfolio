9a4b9077d11888d6e821c6526cd23fd22b48fe08
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-05-22 15:15:55 +0200

    animating

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,35 +1,69 @@
 import React from "react"
 import "./About.css"
-import { useState, useRef, useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 
 export default function About() {
-    const [isIntersecting, setIsIntersecting] = useState(false)
-    const ref = useRef(null)
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                setIsIntersecting(entry.isIntersecting)
-            },
-            {rootMargin: "-150px"}
-        );
-        observer.observe(ref.current);
-        return () => observer.disconnect()
-    }, [])
 
-    useEffect(() => {
-        const refNode = ref.current.querySelector("div")
-        if (isIntersecting) {
-            refNode.classList.add("slideIn")
-        } else {
-            refNode.classList.remove("slideIn")
-        }
-    }, [isIntersecting])
+    useLayoutEffect(() => {
+        // create our context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
+        let ctx = gsap.context(() => {
+
+            //animating heading
+            gsap.set(".section--about .container .section--heading", {y: "-=40", opacity: 0});
+            gsap.to(".section--about .container .section--heading", { 
+                y: 0,
+                opacity: 1,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: ".section--about .container",
+                    toggleActions: "play none play reset"
+                }
+            })
+
+            //animating text
+            gsap.set(".about--text", {y: "-=30", opacity: 0});
+            gsap.to(".about--text", { 
+                y: 0,
+                opacity: 1,
+                duration: 1.5,
+                scrollTrigger: {
+                    trigger: ".section--about .container .section--heading",
+                    toggleActions: "play none play reset"
+                }
+            })
+
+            //animating image
+            gsap.set(".about--image", {filter: 'grayscale(0%)'});
+            gsap.to(".about--image", { 
+                filter: 'grayscale(100%)',
+                duration: 1.5,
+                scrollTrigger: {
+                    trigger: ".about--text",
+                    toggleActions: "play none play reset"
+                }
+            }) 
+            
+            //animating skills
+            gsap.set(".about--skills li", {opacity: 0});
+            gsap.to(".about--skills li", { 
+                opacity: 1,
+                duration: 1,
+                stagger: 0.15,
+                scrollTrigger: {
+                    trigger: ".about--skills",
+                    toggleActions: "play none play reset"
+                }
+            })
+        });  
+        return () => ctx.revert(); // cleanup
+        }, []); // <- empty dependency Array so it doesn't re-run on every render
+
     return (
-        <section ref={ref} id="section--about" className="section--about shapedividers_com-2702">
+        <section id="section--about" className="section--about shapedividers_com-2702">
             <div className="container"> 
                 <h2 className="section--heading"><span className="section--number">01.</span> À propos</h2> 
                 <div className="about--grid">
-                    <div  className="about--text">
+                    <div className="about--text">
                         <p>Bonjour ! Je m'appelle Geoffrey Le Meur et je suis un <span className="about--span">développeur web freelance</span> passionné par la création de sites Internet offrant une expérience utilisateur exceptionnelle. 
                         Également chef d'une petite entreprise dans l'hôtellerie, je me spécialise dans la <span className="about--span">création de sites vitrines pour PME</span> afin d'offrir à mes clients la visibilité qu'ils méritent.</p>
                         <p>Geek depuis mon enfance, mon intérêt pour le développement s'est matérialisé en 2022 lorsque j'ai pris en main le site Internet de mon entreprise suite au départ de notre développeur. 
