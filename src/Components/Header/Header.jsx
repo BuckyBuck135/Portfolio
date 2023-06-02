41bcda07641bba9226dafb154fac2c3f60bc2c18
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-06-02 15:57:43 +0200

    adding new section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from "react"
 import { useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserGraduate, faLaptopFile, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUserGraduate, faLaptopFile, faScrewdriverWrench, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import "./Header.css"
-
 export default function Header() {
     const [isDesktop, setIsDesktop] = React.useState(window.innerWidth > 768)
 
@@ -25,6 +24,7 @@ export default function Header() {
                 <div>
                     <ul className="nav--buttons">
                         <li><a href="#section--about" className="nav--button" aria-label="Lien vers la section à propos"><FontAwesomeIcon icon={faUserGraduate} /></a></li>
+                        <li><a href="#section--skills" className="nav--button" aria-label="Lien vers la section projets"><FontAwesomeIcon icon={faScrewdriverWrench} /></a></li>
                         <li><a href="#section--projects" className="nav--button" aria-label="Lien vers la section projets"><FontAwesomeIcon icon={faLaptopFile} /></a></li>
                         <li><a href="#section--contact" className="nav--button" aria-label="Lien vers la section contact"><FontAwesomeIcon icon={faEnvelope} /></a></li>
                     </ul>
@@ -32,8 +32,9 @@ export default function Header() {
                 <div className="nav--links">
                     <ul>
                         <li><a href="#section--about" className="nav--text"><span className="nav--span">01.</span>A propos</a></li>
-                        <li><a href="#section--projects" className="nav--text"><span className="nav--span">02.</span>Projets</a></li>
-                        <li><a href="#section--contact" className="nav--text"><span className="nav--span">03.</span>Contact</a></li>
+                        <li><a href="#section--skills" className="nav--text"><span className="nav--span">02.</span>Compétences</a></li>
+                        <li><a href="#section--projects" className="nav--text"><span className="nav--span">03.</span>Projets</a></li>
+                        <li><a href="#section--contact" className="nav--text"><span className="nav--span">04.</span>Contact</a></li>
                     </ul>
                 </div>
             </nav>
