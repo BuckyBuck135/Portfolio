5b71f331e9a1bd2a44b1f4bc71394eb8aadf1a7d
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2023-06-02 15:57:00 +0200

    aria labels

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -11,17 +11,17 @@ export default function Aside() {
             <div className="aside--container aside--container-left fadeInUp delay">
                 <div className="aside--inner">
                     <div className="aside--icon-wrapper">
-                        <a className="aside--icon" href="https://www.linkedin.com/in/geoffreylemeur/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-                        <a className="aside--icon" href="https://github.com/BuckyBuck135" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /></a>
-                        <a className="aside--icon" href="https://www.instagram.com/geoffrey.lm.dev/" target="_blank"><FontAwesomeIcon icon={faInstagramSquare} /></a>
-                        <a className="aside--icon" href="https://twitter.com/BuckyBuck135" target="_blank"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+                        <a aria-label="linkedIn" className="aside--icon" href="https://www.linkedin.com/in/geoffreylemeur/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                        <a aria-label="github" className="aside--icon" href="https://github.com/BuckyBuck135" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /></a>
+                        <a aria-label="instagram" className="aside--icon" href="https://www.instagram.com/geoffrey.lm.dev/" target="_blank"><FontAwesomeIcon icon={faInstagramSquare} /></a>
+                        <a aria-label="twitter" className="aside--icon" href="https://twitter.com/BuckyBuck135" target="_blank"><FontAwesomeIcon icon={faTwitterSquare} /></a>
                     </div>
                 </div>
             </div>
 
             <div className="aside--container aside--container-right fadeInUp delay">
                 <div className="aside--inner">
-                    <a className="aside--a" href="mailto:geoffrey.lemeur@pm.me">geoffrey.lemeur@pm.me</a>
+                    <a aria-label="email" className="aside--a" href="mailto:geoffrey.lemeur@pm.me">geoffrey.lemeur@gmail.com</a>
                 </div>
             </div>
         </div>
