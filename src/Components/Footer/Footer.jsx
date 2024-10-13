7efbff2b178e9a909b0e1e8e5ed1b1e6a9e9aa2c
Geoffrey le Meur <geoffrey.lemeur@gmail.com>
2024-10-13 09:36:39 +0200

    moved scripts to bottom oof body

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,5 @@
 import React from "react"
 import "./Footer.css"
-import { useLayoutEffect } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithubSquare, faInstagramSquare, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -18,10 +15,10 @@ export default function Footer() {
         </a>
         <p>Copyright © 2024, Geoffrey Le Meur.</p> 
         <div className="footer--icon-wrapper">
-            <a className="aside--icon" href="https://www.linkedin.com/in/geoffreylemeur/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a className="aside--icon" href="https://github.com/BuckyBuck135" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /></a>
-            <a className="aside--icon" href="https://www.instagram.com/geoffrey.lm.dev/" target="_blank"><FontAwesomeIcon icon={faInstagramSquare} /></a>
-            <a className="aside--icon" href="https://twitter.com/BuckyBuck135" target="_blank"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+            <a aria-label="LinkedIn" className="aside--icon" href="https://www.linkedin.com/in/geoffreylemeur/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"/></svg></a>
+            <a aria-label="GitHub" className="aside--icon" href="https://github.com/BuckyBuck135" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24"><path fill="white" d="M18.88 1.099Q17.78-.001 16.233 0H3.746Q2.198 0 1.099 1.099Q-.001 2.199 0 3.746v12.487q0 1.548 1.099 2.647q1.1 1.1 2.647 1.099H6.66q.285 0 .429-.02a.5.5 0 0 0 .286-.169q.143-.15.143-.435l-.007-.885q-.006-.846-.006-1.34l-.3.052q-.285.052-.721.046a5.6 5.6 0 0 1-.904-.091a2 2 0 0 1-.872-.39a1.65 1.65 0 0 1-.572-.8l-.13-.3a3.3 3.3 0 0 0-.41-.663q-.28-.364-.566-.494l-.09-.065a1 1 0 0 1-.17-.156a.7.7 0 0 1-.117-.182q-.039-.092.065-.15q.104-.06.378-.059l.26.04q.26.051.643.311a2.1 2.1 0 0 1 .631.677q.3.532.722.813q.423.28.852.28t.742-.065a2.6 2.6 0 0 0 .585-.196q.117-.871.637-1.34a9 9 0 0 1-1.333-.234a5.3 5.3 0 0 1-1.223-.507a3.5 3.5 0 0 1-1.047-.872q-.416-.52-.683-1.365q-.266-.846-.266-1.952q0-1.573 1.027-2.68q-.48-1.183.091-2.652q.378-.118 1.119.175t1.086.5q.345.21.553.352a9.2 9.2 0 0 1 2.497-.338q1.288 0 2.498.338l.494-.312a7 7 0 0 1 1.197-.572q.689-.26 1.054-.143q.585 1.47.103 2.653q1.028 1.105 1.028 2.68q0 1.106-.267 1.957q-.266.852-.689 1.366a3.7 3.7 0 0 1-1.053.865q-.63.351-1.223.507a9 9 0 0 1-1.333.235q.675.585.676 1.846v3.11q0 .22.065.357a.36.36 0 0 0 .208.189q.143.052.254.064q.111.014.318.013h2.914q1.548 0 2.647-1.099t1.099-2.647V3.746q0-1.548-1.1-2.647z"/></svg></a>
+            <a aria-label="Instagram" className="aside--icon" href="https://www.instagram.com/geoffrey.lm.dev/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg></a>
+            <a aria-label="Twitter" className="aside--icon" href="https://twitter.com/BuckyBuck135" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="white"><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12.001 2.5c4.478 0 6.717 0 8.108 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.717 0-8.109-1.391c-1.39-1.392-1.39-3.63-1.39-8.109"/><path d="m7 17l4.194-4.193M17 7l-4.193 4.194m0 0L9.777 7H7l4.194 5.807m1.613-1.614L17 17h-2.778l-3.028-4.193"/></g></svg></a>
         </div>
 
     </footer>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,18 @@
 import React from "react"
-import { useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserGraduate, faLaptopFile, faScrewdriverWrench, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import "./Header.css"
 export default function Header() {
-    const [isDesktop, setIsDesktop] = React.useState(window.innerWidth > 768)
-
-    function updateLogo() {
-        setIsDesktop(window.innerWidth > 768)
-    }
-
-    useEffect(() => {
-        window.addEventListener("resize", updateLogo);
-        return () => window.removeEventListener("resize", updateLogo);
-    })
 
     return (
         <header id="header">
             <nav>
-                <div className="nav--logo">
-                    {isDesktop ? (<img className="logo" src="logo-accent.svg" alt="logo" />) : (<img className="logo" src="logo.svg" alt="logo" />)}
-                </div>
                 <div>
                     <ul className="nav--buttons">
-                        <li><a href="#section--about" className="nav--button" aria-label="Lien vers la section à propos"><FontAwesomeIcon icon={faUserGraduate} /></a></li>
-                        <li><a href="#section--skills" className="nav--button" aria-label="Lien vers la section projets"><FontAwesomeIcon icon={faScrewdriverWrench} /></a></li>
-                        <li><a href="#section--projects" className="nav--button" aria-label="Lien vers la section projets"><FontAwesomeIcon icon={faLaptopFile} /></a></li>
-                        <li><a href="#section--contact" className="nav--button" aria-label="Lien vers la section contact"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+                        <li><a href="#section--about" className="nav--button" aria-label="à propos"><FontAwesomeIcon icon={faUserGraduate} /></a></li>
+                        <li><a href="#section--skills" className="nav--button" aria-label="projets"><FontAwesomeIcon icon={faScrewdriverWrench} /></a></li>
+                        <li><a href="#section--projects" className="nav--button" aria-label="projets"><FontAwesomeIcon icon={faLaptopFile} /></a></li>
+                        <li><a href="#section--contact" className="nav--button" aria-label="contact"><FontAwesomeIcon icon={faEnvelope} /></a></li>
                     </ul>
                 </div>
                 <div className="nav--links">
